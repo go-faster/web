a275a6bb856da57bd82ea5534e44f475b4358bc3
Aleksandr Razumov <ernado@ya.ru>
2021-11-09 14:50:36 +0300

    feat(projects.list): add links to logos

--- src/components/ProjectLogo.tsx
+++ src/components/ProjectLogo.tsx
@@ -4,6 +4,7 @@ import styles from './ProjectLogo.module.css';
 type LogoProps = {
     src: string;
     alt?: string;
+    link?: string;
     height?: number;
     width?: number;
 };
@@ -11,8 +12,13 @@ type LogoProps = {
 // LogoSize is default ProjectLogo size.
 export const LogoSize = 100;
 
-export const ProjectLogo = ({src, alt="logo", height=LogoSize, width}: LogoProps): JSX.Element => {
-    return <div className={styles.logo}>
-        <img alt={alt} src={src} height={height} width={width}/>
-    </div>
+export const ProjectLogo = ({src, link, alt="logo", height=LogoSize, width}: LogoProps): JSX.Element => {
+    let Image = () => (<img alt={alt} src={src} height={height} width={width}/>)
+
+    if (link) {
+        Image = () => (<a href={link} about="Project link" target="_blank">
+            <img alt={alt} src={src} height={height} width={width}/>
+        </a>)
+    }
+    return <div className={styles.logo}><Image/></div>
 }
